4f32c64df89346aa31dc9dc9c075e11fe0cff768
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-11-12 00:58:03 +0100

    ✨ add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { ParallaxBannerLayer, ParallaxBanner } from "react-scroll-parallax"
+import img1 from "/1.png"
+
 function App() {
   return (
     <>
+      <ParallaxBanner className="h-screen">
+        <ParallaxBannerLayer image={img1} speed={-16} className="z-0"/>
+      </ParallaxBanner>
+      <div>
+        <div className="flex justify-between items-center px-5 py-10 fixed top-0 w-full mix-blend-difference lg:px-[3.25rem] lg:py-15">
+            <a href="#" className="button-md text-white mix-blend-difference">mempo</a>
+            <div className="flex flex-row gap-x-5 items-center mix-blend-difference lg:gap-x-[3.75rem]">
+              <a href="#" className="button-md text-white mix-blend-difference">link 01</a>
+              <a href="#" className="button-md text-white mix-blend-difference">link 02</a>
+            </div>
+        </div>
+        <p className="absolute top-72 text-center text-neutral-0 heading-3 lg:heading-1 lg:top-60">Mempo masks</p>
+      </div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import { ParallaxProvider } from "react-scroll-parallax"
+import Lenis from "@studio-freight/lenis"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const lenis = new Lenis()
+
+function raf(time: unknown) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ParallaxProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ParallaxProvider>,
 )
